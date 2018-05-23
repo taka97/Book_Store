498a691de210c63770fe28adf38204a20210bb84
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-05-23 14:30:48 +0700

    Add models

---
+++ models/book.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var BookSchema = new Schema(
+  {
+    title: {type: String, required: true, maxlength: 200},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    publisher: {type: Schema.Type.ObjectId, ref: 'Publisher', required: true},
+    publishDate: {type: Date, required: true},
+    price: {type: Number, required: true},
+    genre: {type: Schema.Types.ObjectId, ref: 'Genre', required: true},
+    imageCover: {type: String, required: true}
+  }
+)
+
+BookSchema
+  .virtual('url')
+  .get(function () {
+    return '/book/' + this._id
+  })
+
+// Export model
+module.exports = mongoose.model('Book', BookSchema)
