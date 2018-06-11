16704f92d822decec57450bffce795ced2664991
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-12 00:02:08 +0700

    Make folder for login page

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,6 @@
 // GET Login page
 exports.login = function (req, res, next) {
-  res.render('login', {
+  res.render('login/login', {
     layout: 'layoutLogin',
     title: 'Đăng nhập hoặc Đăng ký tài khoản'
   })
