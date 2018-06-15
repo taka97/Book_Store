92a403f4a52bca19dadb643d562123bd6d5ca52c
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-16 00:41:50 +0700

    Add router for all href in admin page

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-const authorAdminRouter = require('../routes/authorAdmin')
-
 const adminController = require('../controllers/adminController')
+
+const authorAdminRouter = require('../routes/authorAdmin')
+const publisherAdminRouter = require('../routes/publisherAdmin')
+const genreAdminRouter = require('../routes/genreAdmin')
+const bookAdminRouter = require('../routes/bookAdmin')
 
 /* GET admin homepage. */
 router.get('/', adminController.getHomepage)
 
 /* Make middleware for /admin/author */
 router.use('/author', authorAdminRouter)
+
+/* Make middleware for /admin/publisher */
+router.use('/publisher', publisherAdminRouter)
+
+/* Make middleware for /admin/genre */
+router.use('/genre', genreAdminRouter)
+
+/* Make middleware for /admin/book */
+router.use('/book', bookAdminRouter)
 
 module.exports = router
