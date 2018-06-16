16ab76846b4b112f70d0fcfcd22fe9c731c87bf8
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-17 00:02:34 +0700

    Add router for bookDetail, cart and genre (for developer)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var router = express.Router()
+var indexController = require('../controllers/indexController')
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  // res.redirect('/book')
-  res.render('homepage', {title: 'Nhà sách - Trang chủ', layout: 'layoutHomepage'})
+/* GET homepage. */
+router.get('/', indexController.getHomepage)
+
+router.get('/cart', indexController.getCart)
+
+router.get('/genre', indexController.getGenre)
+
+// for developer
+router.get('/bookdetail', (req, res, next) => {
+  res.render('bookDetailPage', {
+    layout: 'layoutHomepage',
+    title: 'Chi tiết sách'})
 })
 
 module.exports = router
