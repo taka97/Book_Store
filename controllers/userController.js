2c62207e6c9a1d7633ea03f44810283e292d2f42
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-16 16:54:38 +0700

    Update router for user page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,41 @@
 // GET user homepage
-
 exports.getHomepage = function (req, res, next) {
   // Successful, so render.
   res.render('account/userHomepage', {
     layout: 'layoutUser',
     title: 'Người dùng'
   })
+}
+
+// GET view profile page
+exports.getViewProfile = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/viewProfile', {
+    layout: 'layoutUser',
+    title: 'Hồ sơ cá nhân'
+  })
+}
+
+// GET change profile page
+exports.getChangeProfile = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/changeProfile', {
+    layout: 'layoutUser',
+    title: 'Thay đổi hồ sơ cá nhân'
+  })
+}
+
+// GET order page
+exports.getOrderPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/order', {
+    layout: 'layoutUser',
+    title: 'Quản lý đơn hàng'
+  })
+}
+
+/* Get logout request */
+exports.logout = function (req, res, next) {
+  // Successful, so render.
+  res.redirect('/login')
 }
