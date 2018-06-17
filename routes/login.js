604cc131a77a74f11e6c1ebfcb73b52d6e8df592
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-18 01:01:43 +0700

    Add package scurf to app

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const csrf = require('csurf')
 const router = express.Router()
 const loginController = require('../controllers/loginController')
+const csrfProtection = csrf()
+router.use(csrfProtection)
 
-// GET LogIn page
+// GET Login page
 router.get('/', loginController.login)
 
 module.exports = router
