1ad837a228c6e513ecf6fdb13c5a41b9b68263f4
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-17 21:49:58 +0700

    Add MVC for making layout Author

--- controllers/authorAdminController.js
+++ controllers/authorAdminController.js
@@ -6,3 +6,40 @@ exports.getHomepage = function (req, res, next) {
     title: 'Quản lý tác giả'
   })
 }
+
+// for developer
+// GET view author (admin) page
+exports.getViewPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/authorView', {
+    layout: 'layoutAdmin',
+    title: 'Xem thông tin tác giả'
+  })
+}
+
+// GET add author (admin) page
+exports.getAddPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/authorAdd', {
+    layout: 'layoutAdmin',
+    title: 'Thêm tác giả'
+  })
+}
+
+// GET edit author (admin) page
+exports.getEditPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/authorEdit', {
+    layout: 'layoutAdmin',
+    title: 'Chỉnh sửa tác giả'
+  })
+}
+
+// GET delete author (admin) page
+exports.getDeletePage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/authorDelete', {
+    layout: 'layoutAdmin',
+    title: 'Xóa tác giả'
+  })
+}
--- routes/authorAdmin.js
+++ routes/authorAdmin.js
@@ -6,4 +6,17 @@ const authorAdminController = require('../controllers/authorAdminController')
 /* GET admin/author homepage. */
 router.get('/', authorAdminController.getHomepage)
 
+// for developer
+// GET view author (admin) page
+router.get('/view', authorAdminController.getViewPage)
+
+// GET add author (admin) page
+router.get('/add', authorAdminController.getAddPage)
+
+// GET edit author (admin) page
+router.get('/edit', authorAdminController.getEditPage)
+
+// GET delete author (admin) page
+router.get('/delete', authorAdminController.getDeletePage)
+
 module.exports = router
