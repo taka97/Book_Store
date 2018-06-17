0d777d98c7e3c0f65f2f03fab167cb79eb7c2161
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-17 22:17:37 +0700

    Add MVC for making layout Genre

--- routes/genreAdmin.js
+++ routes/genreAdmin.js
@@ -6,4 +6,17 @@ const genreAdminController = require('../controllers/genreAdminController')
 /* GET admin/genre homepage. */
 router.get('/', genreAdminController.getHomepage)
 
+// for developer
+// GET view genre (admin) page
+router.get('/view', genreAdminController.getViewPage)
+
+// GET add genre (admin) page
+router.get('/add', genreAdminController.getAddPage)
+
+// GET edit genre (admin) page
+router.get('/edit', genreAdminController.getEditPage)
+
+// GET delete genre (admin) page
+router.get('/delete', genreAdminController.getDeletePage)
+
 module.exports = router
