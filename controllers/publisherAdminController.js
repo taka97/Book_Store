4567ebcc8e12010ca780da242f7ea4d895c6e2f4
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-17 22:21:06 +0700

    Add MVC for making layout Publisher

--- controllers/publisherAdminController.js
+++ controllers/publisherAdminController.js
@@ -6,3 +6,40 @@ exports.getHomepage = function (req, res, next) {
     title: 'Quản lý nhà xuất bản'
   })
 }
+
+// for developer
+// GET view publisher (admin) page
+exports.getViewPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/publisherView', {
+    layout: 'layoutAdmin',
+    title: 'Xem thông tin sách'
+  })
+}
+
+// GET add publisher (admin) page
+exports.getAddPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/publisherAdd', {
+    layout: 'layoutAdmin',
+    title: 'Thêm sách'
+  })
+}
+
+// GET edit publisher (admin) page
+exports.getEditPage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/publisherEdit', {
+    layout: 'layoutAdmin',
+    title: 'Chỉnh sửa sách'
+  })
+}
+
+// GET delete publisher (admin) page
+exports.getDeletePage = function (req, res, next) {
+  // Successful, so render.
+  res.render('account/publisherDelete', {
+    layout: 'layoutAdmin',
+    title: 'Xóa sách'
+  })
+}
--- routes/publisherAdmin.js
+++ routes/publisherAdmin.js
@@ -6,4 +6,17 @@ const publisherAdminController = require('../controllers/publisherAdminControlle
 /* GET admin/publisher homepage. */
 router.get('/', publisherAdminController.getHomepage)
 
+// for developer
+// GET view publisher (admin) page
+router.get('/view', publisherAdminController.getViewPage)
+
+// GET add publisher (admin) page
+router.get('/add', publisherAdminController.getAddPage)
+
+// GET edit publisher (admin) page
+router.get('/edit', publisherAdminController.getEditPage)
+
+// GET delete publisher (admin) page
+router.get('/delete', publisherAdminController.getDeletePage)
+
 module.exports = router
