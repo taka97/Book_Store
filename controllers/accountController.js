ecda4de7e10ab64b8a1c98bdaef68228ef11315c
Võ Sơn Hiệp <33183336+vosonhiep@users.noreply.github.com>
2018-06-17 19:40:40 +0700

    repair width, translate

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,7 +1,7 @@
 // GET account homepage
 exports.getHomepage = function (req, res, next) {
   // Successful, so render.
-  res.render('account/accountManager', {
+  res.render('account/accountHomepage', {
     layout: 'layoutAdmin',
     title: 'Quản lý tài khoản'
   })
