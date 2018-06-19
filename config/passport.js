226c50c83e3afcace1b8ae3a99124ab47c363a0c
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-20 00:22:09 +0700

    Add autho method

--- config/passport.js
+++ config/passport.js
@@ -13,3 +13,25 @@ passport.deserializeUser(function (id, done) {
     })
 })
 
+passport.use('signup', new LocalStratery({
+  usernameField: 'email',
+  passwordField: `password`,
+  passReqToCallback: true
+}, function (req, email, password, done) {
+  Account.findOne({ 'email': email })
+    .exec(function (err, account) {
+      if (err) {
+        return done(err)
+      }
+      if (account) {
+        return done(null, false, { message: 'Email đang được sử dụng trong 1 tài khoản khác' })
+      }
+      var newAccount = new Account()
+      newAccount.email = email
+      newAccount.password = newAccount.encryptPassword(password)
+      newAccount.save(function (err, result) {
+        if (err) { return done(err) }
+        return done(null, account)
+      })
+    })
+}))
