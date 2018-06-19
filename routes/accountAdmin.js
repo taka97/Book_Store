b9ffa3d0efd75374a1b0d17ef0713530d0cf0e9f
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-19 22:30:21 +0700

    Add router for /admin/account

--- routes/accountAdmin.js
+++ routes/accountAdmin.js
@@ -6,4 +6,16 @@ const accountController = require('../controllers/accountController')
 /* GET account page. */
 router.get('/', accountController.getHomepage)
 
+/* GET view account (admin) page */
+router.get('/view', accountController.getViewPage)
+
+/* GET upgrade account (admin) page */
+router.get('/upgrade', accountController.getUpgradePage)
+
+/* GET delete account (admin) page */
+router.get('/delete', accountController.getDeletePage)
+
+/* GET block account (admin) page */
+router.get('/block', accountController.getBlockPage)
+
 module.exports = router
