9b7b2d1ba4272ba1593b428f77a38a0500503c38
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-23 01:51:07 +0700

    Update comment for all router

--- routes/login.js
+++ routes/login.js
@@ -8,10 +8,13 @@ router.use(csrfProtection)
 // GET Login page
 router.get('/login', loginController.getLoginPage)
 
+// GET Register page
 router.get('/register', loginController.getRegisterPage)
 
+// POST request to login
 router.post('/login', loginController.postLogin)
 
+// POST request to register
 router.post('/register', loginController.postRegister)
 
 module.exports = router
