6947b57dffe446122525ff62e180c24140ff0bd0
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-24 00:06:47 +0700

    Add loginProtected

--- routes/login.js
+++ routes/login.js
@@ -2,19 +2,20 @@ const express = require('express')
 const csrf = require('csurf')
 const router = express.Router()
 const loginController = require('../controllers/loginController')
+const loginProtected = require('../controllers/loginProtected')
 const csrfProtection = csrf()
 router.use(csrfProtection)
 
 // GET Login page
-router.get('/login', loginController.getLoginPage)
+router.get('/login', loginProtected.notLoggedIn, loginController.getLoginPage)
 
 // GET Register page
-router.get('/register', loginController.getRegisterPage)
+router.get('/register', loginProtected.notLoggedIn, loginController.getRegisterPage)
 
 // POST request to login
-router.post('/login', loginController.postLogin)
+router.post('/login', loginProtected.notLoggedIn, loginController.postLogin)
 
 // POST request to register
-router.post('/register', loginController.postRegister)
+router.post('/register', loginProtected.notLoggedIn, loginController.postRegister)
 
 module.exports = router
