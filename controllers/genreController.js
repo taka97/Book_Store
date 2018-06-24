77e14349ae48cc0eead79be255f6916454445980
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-25 03:00:17 +0700

    make controller render with layout

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -4,6 +4,29 @@ const Author = require('../models/author')
 const Genre = require('../models/genre')
 const Publisher = require('../models/publisher')
 
+// Get list all genre in database
+exports.listGenres = function (req, res, next) {
+  async.parallel({
+    listGenres: (callback) => {
+      Genre.find({})
+        .exec(callback)
+    }
+  }, (err, results) => {
+    if (err) { return next(err) }
+    // Successful, so render.
+    res.render('genre', {
+      layout: 'layoutHomepage',
+      title: 'Book Store',
+      listGenres: results.listGenres,
+      listAuthors: results.listAuthors,
+      listPublishers: results.listPublisher,
+      listBooks: results.listBooks
+    })
+
+    console.log(results.listBooks)
+  })
+}
+
 // Get list all book in database
 exports.genreDetail = function (req, res, next) {
   async.parallel({
@@ -33,6 +56,7 @@ exports.genreDetail = function (req, res, next) {
     }
     // Successful, so render.
     res.render('book', {
+      layout: 'layoutHomepage',
       title: 'Book Store',
       listGenres: results.listGenres,
       listAuthors: results.listAuthors,
