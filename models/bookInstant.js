0765c65e79c2c2057147e5a8b3a8c552b11968b8
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-25 00:48:31 +0700

    Add model BookInstant

---
+++ models/bookInstant.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var BookInstantSchema = new Schema(
+  {
+    book: { type: Schema.ObjectId, ref: 'Book', required: true }, // reference to the associated book
+    currentPrice: { type: Number, required: true },
+    currentTotalQuantity: { type: Number, required: true },
+    size: { type: [Number, Number] },
+    coverType: { type: String, required: true },
+    status: { type: String, enum: ['Available', 'Sold'], required: true }
+  }
+)
+
+BookInstantSchema
+  .virtual('url')
+  .get(function () {
+    return '/bookinstant/' + this._id
+  })
+
+// Export model
+module.exports = mongoose.model('BookInstant', BookInstantSchema)
