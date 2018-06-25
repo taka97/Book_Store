81f1f6940f5debe9f50b3ff7451ec1975f00ea01
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-26 03:42:07 +0700

    Rename instance

--- models/bookInstance.js
+++ models/bookInstance.js
@@ -9,7 +9,7 @@ var BookInstanceSchema = new Schema(
     currentTotalQuantity: { type: Number, required: true },
     size: { type: [Number, Number] },
     coverType: { type: String, required: true },
-    imageCover: { type: [String, String], required: true },
+    imageCover: { type: [String], required: true },
     status: { type: String, enum: ['Còn hàng', 'Hết hàng'], required: true }
   }
 )
