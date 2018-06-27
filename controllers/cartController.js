a27afd4ec49d148618874421c2d8187080eca0c7
Hồ Văn Hoàng <vanhoang0609@gmail.com>
2018-06-28 03:32:42 +0700

    Solve some problem

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,35 +6,60 @@ const Book = require('../models/book')
 
 /* GET cart page */
 exports.getCartPage = function (req, res, next) {
-  async.parallel({
-    listGenres: (callback) => {
-      Genre.find()
-        .exec(callback)
-    }
-  }, (err, results) => {
-    if (err) { return next(err) }
-    var GenreChucks = []
-    var chunkSize = 3
-    for (var i = 0; i <= results.listGenres.length; i += chunkSize) {
-      GenreChucks.push(results.listGenres.slice(i, i + chunkSize))
-    }
-    var cartDetail = new Cart(req.session.cart ? req.session.cart : {})
-    // success so render
-    res.render('cartPage', {
-      layout: 'layoutHomepage',
-      title: 'Giỏ hàng',
-      csrfToken: req.csrfToken(),
-      GenreChucks: GenreChucks,
-      cartDetail: cartDetail.generateArray()
+  if (!req.session.cart) {
+    async.parallel({
+      listGenres: (callback) => {
+        Genre.find()
+          .exec(callback)
+      }
+    }, (err, results) => {
+      if (err) { return next(err) }
+      var GenreChucks = []
+      var chunkSize = 3
+      for (var i = 0; i <= results.listGenres.length; i += chunkSize) {
+        GenreChucks.push(results.listGenres.slice(i, i + chunkSize))
+      }
+      // success so render
+      res.render('cartPage', {
+        layout: 'layoutHomepage',
+        title: 'Giỏ hàng',
+        csrfToken: req.csrfToken(),
+        GenreChucks: GenreChucks,
+        cartDetail: null
+      })
+      // console.log(cartDetail.generateArray())
     })
-    console.log(cartDetail.generateArray())
-  })
+  } else {
+    async.parallel({
+      listGenres: (callback) => {
+        Genre.find()
+          .exec(callback)
+      }
+    }, (err, results) => {
+      if (err) { return next(err) }
+      var GenreChucks = []
+      var chunkSize = 3
+      for (var i = 0; i <= results.listGenres.length; i += chunkSize) {
+        GenreChucks.push(results.listGenres.slice(i, i + chunkSize))
+      }
+      var cartDetail = new Cart(req.session.cart)
+      // success so render
+      res.render('cartPage', {
+        layout: 'layoutHomepage',
+        title: 'Giỏ hàng',
+        csrfToken: req.csrfToken(),
+        GenreChucks: GenreChucks,
+        cartDetail: cartDetail.generateArray()
+      })
+      // console.log(cartDetail.generateArray())
+    })
+  }
 }
 
 /* GET add-to-cart page */
 exports.postAddToCart = function (req, res, next) {
   var productId = req.body.product_id
-  var cart = new Cart(req.session.cart ? req.session.cart : {})
+  var cart = new Cart(req.session.cart)
   async.parallel({
     one: function (callback) {
       BookInstance.findById(productId)
@@ -74,4 +99,8 @@ exports.postAddToCart = function (req, res, next) {
     console.log(cart)
     res.redirect('/book')
   })
+}
+
+exports.getCheckoutPage = function (req, res, next) {
+
 }
