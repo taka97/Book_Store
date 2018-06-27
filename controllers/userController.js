1fd2754fa8045b1e1f292fd8cfe3500a7685ae11
Võ Sơn Hiệp <1512170@student.hcmus.edu.vn>
2018-06-27 17:43:00 +0700

    post and get changeProfile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,64 @@
+const async = require('async')
+const Account = require('../models/account')
+
 // GET user homepage
 exports.getHomepage = function (req, res, next) {
-  // Successful, so render.
-  res.render('account/userHomepage', {
-    layout: 'layoutUser',
-    title: 'Người dùng'
+  async.parallel({
+    listAccounts: (callback) => {
+      Account.find()
+        .exec(callback)
+    }
+  }, (err, results) => {
+    if (err) { return next(err) }
+    // Successful, so render.
+    res.render('account/userHomepage', {
+      layout: 'layoutUser',
+      title: 'Người dùng',
+      listAccounts: results.listAccounts
+    })
   })
+  console.log('listAccount:' + results.listAccounts)
 }
 
 // GET view profile page
 exports.getViewProfile = function (req, res, next) {
-  // Successful, so render.
-  res.render('account/viewProfile', {
-    layout: 'layoutUser',
-    title: 'Hồ sơ cá nhân',
-    user: req.user
+  async.parallel({
+    accountDetail: (callback) => {
+      Account.findById(req.params.id)
+        .exec(callback)
+    }
+  }, (err, results) => {
+    if (err) { return next(err) }
+    // Successful, so render.
+    res.render('account/viewProfile', {
+      layout: 'layoutUser',
+      title: 'Hồ sơ cá nhân',
+      user: req.user,
+      account: results.accountDetail
+    })
+    console.log(req.user),
+      console.log('account: ' + results.accountDetail)
   })
-  console.log(req.user)
 }
 
 // GET change profile page
 exports.getChangeProfile = function (req, res, next) {
-  // Successful, so render.
-  res.render('account/changeProfile', {
-    layout: 'layoutUser',
-    title: 'Thay đổi hồ sơ cá nhân'
+  async.parallel({
+    accountDetail: (callback) => {
+      Account.findById(req.params.id)
+        .exec(callback)
+    }
+  }, (err, results) => {
+    if (err) { return next(err) }
+    // Successful, so render.
+    res.render('account/changeProfile', {
+      layout: 'layoutUser',
+      title: 'Thay đổi hồ sơ cá nhân',
+      csrfToken: req.csrfToken(), // send token to client, it is neccessary when send post request
+      user: req.user,
+      account: results.accountDetail
+    })
+    console.log('account: ' + results.accountDetail)
   })
 }
 
@@ -40,4 +75,27 @@ exports.getOrderPage = function (req, res, next) {
 exports.logout = function (req, res, next) {
   // Successful, so render.
   res.redirect('/login')
+}
+
+// POST change account
+exports.postChangeProfile = function (req, res, next) {
+  var editAccount = new Account({
+    _id: req.params.id,
+    email: req.body.email,
+    verifyEmail: req.params.verifyEmail,
+    password: req.params.password,
+    avatarPath: req.params.avatarPath,
+    name: req.body.name,
+    birthDate: req.body.date,
+    gender: req.body.gender,
+    address: req.body.address,
+    typeAccount: req.params.typeAccount,
+    isBlock: req.params.isBlock
+  })
+  Account.findByIdAndUpdate(req.params.id, editAccount, function (err) {
+    if (err) throw err
+    else {
+      res.redirect('/user/profile')
+    }
+  })
 }
--- routes/accountAdmin.js
+++ routes/accountAdmin.js
@@ -9,6 +9,9 @@ router.get('/', accountAdminController.getHomepage)
 /* GET view account (admin) page */
 router.get('/view', accountAdminController.getViewPage)
 
+/* GET change account (admin) page */
+router.get('/edit', accountAdminController.getEditPage)
+
 /* GET upgrade account (admin) page */
 router.get('/upgrade', accountAdminController.getUpgradePage)
 
@@ -17,5 +20,8 @@ router.get('/delete', accountAdminController.getDeletePage)
 
 /* GET block account (admin) page */
 router.get('/block', accountAdminController.getBlockPage)
+
+/* POST edit account (admin) page */
+// router.get('/edit/:id', accountAdminController.getEditPage)
 
 module.exports = router
